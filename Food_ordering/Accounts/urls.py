899236316842603c1870f89dcb192f30df280a3c
Raj bhoir <bhoirraj872@gmail.com>
2023-08-27 22:06:23 +0530

    Adding new Account App and setting it

--- Food_ordering/Accounts/urls.py
+++ Food_ordering/Accounts/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('',include('HOME.urls')),
-    path('',include('Accounts.urls')),
+    path('Register/',views.User_Register,name='Register'),
     path('admin/', admin.site.urls),
 ]
---
+++ Food_ordering/Accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def User_Register(request):
+    return render(request,'User_Register.html')
