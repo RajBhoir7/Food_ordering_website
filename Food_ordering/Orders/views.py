0678e218b209897dcc24439430e04f1187b24a31
Raj bhoir <bhoirraj872@gmail.com>
2023-09-26 19:14:06 +0530

    26-09-2023

--- Food_ordering/Orders/views.py
+++ Food_ordering/Orders/views.py
@@ -4,7 +4,10 @@ from Restaurant.models import Rest_details
 # Create your views here.
 
 
+
 class Cart(View):
     def get(self,request):
         hotels_in_user_location = Rest_details.objects.all()
         return render(request,'Cart_Page.html',{'data':hotels_in_user_location})
+    
+
